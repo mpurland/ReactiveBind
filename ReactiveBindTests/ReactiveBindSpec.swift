2dd6eca74bc4520691f75dbf8fd3fc6f042ff746
Matthew Purland <m.purland@gmail.com>
2015-12-10 15:00:59 -0800

    Fixed project

---
+++ ReactiveBindTests/ReactiveBindSpec.swift
@@ -0,0 +1,15 @@
+//
+//  ReactiveBindSpec.swift
+//  ReactiveBindTests
+//
+//  Created by Matthew Purland on 12/10/15.
+//  Copyright © 2015 Matthew Purland. All rights reserved.
+//
+
+import XCTest
+@testable import ReactiveBind
+
+class ReactiveBindSpec: XCTestCase {
+    func testBinding() {
+    }
+}
