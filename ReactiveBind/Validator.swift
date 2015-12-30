690f017d82dbc6385a1808cc3594f6c5ba7fa195
Matthew Purland <m.purland@gmail.com>
2015-12-30 12:35:40 -0800

    Improved validators.

--- ReactiveBind/Validator.swift
+++ ReactiveBind/Validator.swift
@@ -1,19 +1,30 @@
 import Foundation
 import ReactiveCocoa
 
+public typealias StringValidatorAction = Action<String, Bool, NoError>
+
+/// Create a validator action from a validator function
+public func validatorAction(validator: String -> Bool) -> StringValidatorAction {
+    return StringValidatorAction({ (string) -> SignalProducer<Bool, NoError> in
+        let valid = validator(string)
+        return SignalProducer(value: valid)
+    })
+}
+
 /// Property validator
-protocol PropertyValidator {
+public protocol PropertyValidator {
     typealias Value
+    
     func rac_valid(validator: Value -> Bool) -> SignalProducer<Bool, NoError>
 }
 
-protocol ValidatablePropertyType: PropertyType, PropertyValidator {
+public protocol ValidatablePropertyType: PropertyType, PropertyValidator {
 }
 
 /// A property type that is validatable from PropertyValidator
 extension ValidatablePropertyType {
     /// A signal producer that reacts to the text field text changes and uses the given validator and returns if the text is valid.
-    func rac_valid(validator: Value -> Bool) -> SignalProducer<Bool, NoError> {
+    public func rac_valid(validator: Value -> Bool) -> SignalProducer<Bool, NoError> {
         return producer.map(validator)
     }
 }
@@ -22,3 +33,16 @@ extension ValidatablePropertyType {
 extension ConstantProperty: ValidatablePropertyType {}
 extension MutableProperty: ValidatablePropertyType {}
 extension DynamicProperty: ValidatablePropertyType {}
+
+/// Validator extensions for UITextField
+extension UITextField {
+    /// A signal producer that reacts to the text field text changes and uses the given validator and returns if the text is valid.
+    public func rac_valid(validator: String -> Bool) -> SignalProducer<Bool, NoError> {
+        return rac_text.producer.map(validator)
+    }
+    
+    /// A signal producer that reacts to the text field text changes and uses the given validator action and returns if the text is valid.
+    public func rac_valid(validatorAction: Action<String, Bool, NoError>) -> SignalProducer<Bool, NoError> {
+        return rac_text.producer.flatMap(FlattenStrategy.Latest) { validatorAction.apply($0).suppressError() }
+    }
+}
